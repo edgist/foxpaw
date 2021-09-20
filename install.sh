e0686949cfc0705932e4bc5f98ba88c1bd34ee9e
inkubur <komorekalu@outlook.com>
2021-09-20 16:41:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-curl -SL https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64 -o - | sudo tar xvjf - -C /opt
+curl -SL https://download.mozilla.org/\?product\=firefox-latest-ssl\&os\=linux64 -o - | sudo tar xvjf - -C ~/.local/share
 
-sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+sudo ln -s ~/.local/share/firefox/firefox /usr/bin/firefox
 
 sudo curl -SL https://cdn.jsdelivr.net/gh/inkubur/foxpaw/firefox.desktop -o /usr/share/applications/firefox.desktop
